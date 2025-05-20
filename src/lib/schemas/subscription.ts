55761b17c9dfc81347d8d7b0446626d9efd98305
tatiana.razumova <tatiana.razumova@raftds.com>
2025-05-20 22:15:56 +0300

    added subscription scheme type

--- src/lib/schemas/subscription.ts
+++ src/lib/schemas/subscription.ts
@@ -3,3 +3,5 @@ import { z } from 'zod';
 export const subscriptionSchema = z.object({
 	email: z.string().email({ message: 'Invalid email address' })
 });
+
+export type SubscriptionSchema = z.infer<typeof subscriptionSchema>;
