be612232e8549a17d80f2226ce6bf044459da5ab
tatiana.razumova <tatiana.razumova@raftds.com>
2025-05-22 07:05:50 +0300

    schema and seed fix

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,28 +3,65 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 try {
+	const episodeWithPost = await prisma.episode.create({
+		data: {
+			slug: 'episode-23',
+			episodeNumber: 23,
+			category: 'Business',
+			date: new Date('2022-01-01T12:00:00Z'),
+			title: 'How to rapidly test any experience!',
+			abstract:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed mollis augue. Cras suscipit sit amet est in aliquam. In vel blandit nunc. Donec at dolor in orci tristique bibendum.Cras non interdum ligula, sit amet imperdiet purus. Vestibulum quis leo nibh.',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consect adipiscing elit. Sed at arcu dui. Aenean placerat mauris nisl...',
+			transcript: 'Transcript content here...',
+			audio: '',
+			tags: ['#Apple', '#Keynote', '#Product'],
+			posts: {
+				create: [
+					{
+						slug: 'how-to-create-your-own-podcast-cover-art',
+						image: 'https://placedog.net/180/180?random',
+						title: 'How to create your own podcast cover art?',
+						abstract:
+							'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed mollis augue. Cras suscipit sit amet est in aliquam. In vel blandit nunc. Donec at dolor in orci tristique bibendum.ligula, sit amet imperdiet purus. Vestibulum quis leo nibh.',
+						text: 'Vivamus et aliquet neque. Mauris feugiat blandit augue a vestibulum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam a luctus magna, a finibus massa. Proin ultricies, arcu ac dignissim sollicitudin, nibh nibh fermentum eros, id consequat nisi odio vestibulum tortor. Cras non interdum ligula, sit amet imperdiet purus. Vestibulum quis leo nibh. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam a luctus magna, a finibus massa.',
+						tags: ['#Apple', '#Keynote', '#Product']
+					}
+				]
+			}
+		}
+	});
+
 	const episodes = await prisma.episode.createMany({
 		data: [
 			{
-				topic: 'Business',
+				slug: 'episode-21',
+				episodeNumber: 21,
+				category: 'Business',
 				date: new Date('2022-01-01T12:00:00Z'),
-				title: '23 - How to rapidly test any experience!',
+				title: 'Everything you need to know about mind mapping creation',
+				abstract:
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed mollis augue. Cras suscipit sit amet est in aliquam. In vel blandit nunc. Donec at dolor in orci tristique bibendum.Cras non interdum ligula, sit amet imperdiet purus. Vestibulum quis leo nibh.',
 				description:
-					'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consect adipiscing elit. Sed at arcu dui. Aenean placerat mauris nisl...'
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consect adipiscing elit. Sed at arcu dui. Aenean placerat mauris nisl...',
+				transcript: 'Transcript content here...',
+				audio: '',
+				tags: ['#Apple', '#Keynote', '#Product']
 			},
 			{
-				topic: 'Business',
-				date: new Date('2021-01-18T12:00:00Z'),
-				title: '22 - Everything you need to know about mind mapping creation',
+				slug: 'episode-22',
+				episodeNumber: 22,
+				category: 'Business',
+				date: new Date('2022-01-01T12:00:00Z'),
+				title: 'Everything you need to know about mind mapping creation',
+				abstract:
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed mollis augue. Cras suscipit sit amet est in aliquam. In vel blandit nunc. Donec at dolor in orci tristique bibendum.Cras non interdum ligula, sit amet imperdiet purus. Vestibulum quis leo nibh.',
 				description:
-					'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consect adipiscing elit...'
-			},
-			{
-				topic: 'Business',
-				date: new Date('2021-01-18T12:00:00Z'),
-				title: '21 - Makemeup Podcast Theme; launch it now !  ',
-				description:
-					'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consect adipiscing elit...'
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consect adipiscing elit. Sed at arcu dui. Aenean placerat mauris nisl...',
+				transcript: 'Transcript content here...',
+				audio: '',
+				tags: ['#Apple', '#Keynote', '#Product']
 			}
 		]
 	});
@@ -32,14 +69,24 @@ try {
 	const posts = await prisma.post.createMany({
 		data: [
 			{
-				imageUrl: 'https://placedog.net/180/180?random',
+				slug: 'how-to-create-your-own-podcast-cover-art',
+				image: 'https://placedog.net/180/180?random',
 				title: 'How to create your own podcast cover art?',
-				date: new Date('2019-05-18T12:00:00Z')
+				date: new Date('2019-05-18T12:00:00Z'),
+				abstract:
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed mollis augue. Cras suscipit sit amet est in aliquam. In vel blandit nunc. Donec at dolor in orci tristique bibendum.ligula, sit amet imperdiet purus. Vestibulum quis leo nibh.',
+				text: 'Vivamus et aliquet neque. Mauris feugiat blandit augue a vestibulum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam a luctus magna, a finibus massa. Proin ultricies, arcu ac dignissim sollicitudin, nibh nibh fermentum eros, id consequat nisi odio vestibulum tortor. Cras non interdum ligula, sit amet imperdiet purus. Vestibulum quis leo nibh. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam a luctus magna, a finibus massa.',
+				tags: ['#Apple', '#Keynote', '#Product']
 			},
 			{
-				imageUrl: 'https://placedog.net/180/180?random',
+				slug: 'how-to-create-your-own-podcast-cover-art',
+				image: 'https://placedog.net/180/180?random',
 				title: 'How to create your own podcast cover art?',
-				date: new Date('2020-04-11T12:00:00Z')
+				date: new Date('2020-04-11T12:00:00Z'),
+				abstract:
+					'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed mollis augue. Cras suscipit sit amet est in aliquam. In vel blandit nunc. Donec at dolor in orci tristique bibendum.ligula, sit amet imperdiet purus. Vestibulum quis leo nibh.',
+				text: 'Vivamus et aliquet neque. Mauris feugiat blandit augue a vestibulum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam a luctus magna, a finibus massa. Proin ultricies, arcu ac dignissim sollicitudin, nibh nibh fermentum eros, id consequat nisi odio vestibulum tortor. Cras non interdum ligula, sit amet imperdiet purus. Vestibulum quis leo nibh. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam a luctus magna, a finibus massa.',
+				tags: ['#Apple', '#Keynote', '#Product']
 			}
 		]
 	});
@@ -47,27 +94,30 @@ try {
 	const teamMembers = await prisma.teamMember.createMany({
 		data: [
 			{
+				slug: 'aida-cave',
 				name: 'Aida Cave',
 				position: 'Position',
-				imageUrl: 'https://placedog.net/300/300?random',
+				image: 'https://placedog.net/300/300?random',
 				showOnMainPage: true,
 				linkedinLink: '',
 				githubLink: '',
 				twitterLink: ''
 			},
 			{
+				slug: 'nick-carleton',
 				name: 'Nick Carleton',
 				position: 'Position',
-				imageUrl: 'https://placedog.net/300/300?random',
+				image: 'https://placedog.net/300/300?random',
 				showOnMainPage: true,
 				linkedinLink: '',
 				githubLink: '',
 				twitterLink: ''
 			},
 			{
+				slug: 'miles-davis',
 				name: 'Miles Davis',
 				position: 'Jazz player',
-				imageUrl: 'https://placedog.net/300/300?random',
+				image: 'https://placedog.net/300/300?random',
 				showOnMainPage: false,
 				linkedinLink: '',
 				githubLink: '',
@@ -75,6 +125,8 @@ try {
 			}
 		]
 	});
+
+	console.log('EPISODE WITH POST ADDED', !!episodeWithPost);
 	console.log('EPISODES COUNT', episodes.count);
 	console.log('POSTS COUNT', posts.count);
 	console.log('TEAM MEMBERS COUNT', teamMembers.count);
